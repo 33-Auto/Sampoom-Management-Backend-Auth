50e9c3e4ef2187dc177e7eac955fdde18b211fad
LeeJongJin <ljj61111@gmail.com>
2025-11-03 20:22:01 +0900

    [FEAT] 로그인 시 조직 검증 추가_(WIP2: 코드래빗 반영)

--- src/main/java/com/sampoom/auth/api/auth/internal/dto/LoginUserRequest.java
+++ src/main/java/com/sampoom/auth/api/auth/internal/dto/LoginUserRequest.java
@@ -2,7 +2,6 @@ package com.sampoom.auth.api.auth.internal.dto;
 
 import com.sampoom.auth.common.entity.Workspace;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -10,8 +9,8 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@NotNull
 public class LoginUserRequest {
+    @NotNull
     private Long userId;       // Auth에서 생성한 userId
     @Valid
     private Workspace workspace;  // 근무지(대리점, 창고, 공장 등)
